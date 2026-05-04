57176e52625ac33df2f9dcf6b03b3b555c8ca80a
JohnCena1306 <oliverjoostpersson@icloud.com>
2026-05-04 15:37:40 +0200

    EKSAMENSPROJEKT

---
+++ src/main/java/thestudiegruppe/projectestimationtool/Eksamensprojekt2SemApplication.java
@@ -0,0 +1,13 @@
+package thestudiegruppe.projectestimationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamensprojekt2SemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamensprojekt2SemApplication.class, args);
+    }
+
+}
